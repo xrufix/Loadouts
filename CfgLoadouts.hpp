42f98d209ed00249c86e5c8574d003696ce8b22f
rufix <stefan@ruhfass.com>
2017-10-28 01:24:36 +0200

    Added Bundeswehr factions

--- CfgLoadouts.hpp
+++ CfgLoadouts.hpp
@@ -5,15 +5,15 @@ class CommonDefault;
 
 #define SIDE Blufor
 #define SIDE_SHORT b
-#include "loadouts\SYND.hpp"
+#include "loadouts\BW_FLECK.hpp"
 #include "soldiers_BLUFOR.hpp"
 
 #define SIDE Opfor
 #define SIDE_SHORT o
-#include "loadouts\SYND.hpp"
+#include "loadouts\USARMY_OCP.hpp"
 #include "soldiers_OPFOR.hpp"
 
 #define SIDE Independant
 #define SIDE_SHORT i
-#include "loadouts\SYND.hpp"
+#include "loadouts\BW_TROP.hpp"
 #include "soldiers_INDEP.hpp" 
